64afbbd5519f9306749e037b8579d4c6d8daa56f
Mukulshitole <102034299+Mukulshitole@users.noreply.github.com>
2024-03-25 08:25:26 +0530

    sensitivity_update
    Different sensitivity for different zoom prespective

--- Assets/Scripts/ThirdPersonControllerShooter.cs
+++ Assets/Scripts/ThirdPersonControllerShooter.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using StarterAssets;
+using UnityEngine.InputSystem;
 
 public class ThirdPersonControllerShooter : MonoBehaviour
 {
    [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
+    [SerializeField] private float normalSensetivity;  // different sensetivity for different components
+    [SerializeField] private float aimSensetivity;
+
+    private ThirdPersonController thirdPersonController;  // this things helps to call scripts
     private StarterAssetsInputs starterAssetsInputs;
 
     private void Awake()
     {
+        thirdPersonController = GetComponent<ThirdPersonController>(); 
         starterAssetsInputs = GetComponent<StarterAssetsInputs>();
     }
     private void Update()
@@ -18,11 +24,12 @@ public class ThirdPersonControllerShooter : MonoBehaviour
         if (starterAssetsInputs.Aim)
         {
             aimVirtualCamera.gameObject.SetActive(true);
-
+            thirdPersonController.Setsensetivity(aimSensetivity);
         }
         else
         {
             aimVirtualCamera.gameObject.SetActive(false);
+            thirdPersonController.Setsensetivity(normalSensetivity);
         }
         
     }
